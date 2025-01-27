028f0a18794667cbc2ad6dac211869b7adebcedb
Rineta2 <rzkir571@gmail.com>
2025-01-27 21:27:00 +0700

    [update] section

--- src/components/section/home/Home.tsx
+++ src/components/section/home/Home.tsx
@@ -31,30 +31,30 @@ export default function Home() {
                         />
                     </div>
 
-                    <div className="py-6 lg:py-10 flex flex-col gap-6 w-full order-1 lg:order-2 text-center lg:text-left">
-                        <h3 className="text-xl md:text-2xl font-bold">{homeData.text}</h3>
-                        <h1 className="text-4xl md:text-5xl xl:text-5xl font-bold text-primary">{homeData.title}</h1>
-                        <p className="text-base md:text-lg max-w-2xl mx-auto lg:mx-0">{homeData.description}</p>
+                    <div className="py-4 sm:py-6 lg:py-10 flex flex-col gap-4 sm:gap-6 w-full order-1 lg:order-2 text-center lg:text-left z-10">
+                        <h3 className="text-lg sm:text-xl md:text-2xl font-bold">{homeData.text}</h3>
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-primary">{homeData.title}</h1>
+                        <p className="text-sm sm:text-base md:text-lg max-w-2xl mx-auto lg:mx-0">{homeData.description}</p>
                         <Link
                             href={homeData.href}
-                            className="btn bg-primary text-white w-fit mx-auto lg:mx-0 hover:bg-title transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg px-6 py-3 rounded-lg"
+                            className="btn bg-primary text-white w-fit mx-auto lg:mx-0 hover:bg-title transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg px-4 sm:px-6 py-2 sm:py-3 rounded-lg text-sm sm:text-base"
                         >
                             {homeData.name}
                         </Link>
                     </div>
                 </div>
 
-                <div className="absolute bottom-[-50%] left-[50%] w-full h-full flex items-center justify-center z-10">
+                <div className="absolute bottom-[-50%] sm:bottom-[-40%] md:bottom-[-50%] left-[45%] sm:left-[50%] w-full h-full flex items-center justify-center">
                     <a href="#populer" className="relative">
                         <Image
                             src={imgScroll}
                             alt="scroll"
-                            className="object-cover animate-spin infinite ease-in-out w-32 h-32 [filter:sepia(100%)_saturate(10000%)_hue-rotate(230deg)]"
+                            className="object-cover animate-spin infinite ease-in-out w-24 sm:w-28 md:w-32 h-24 sm:h-28 md:h-32 [filter:sepia(100%)_saturate(10000%)_hue-rotate(230deg)]"
                             style={{ animationDuration: '3000ms' }}
                             width={500}
                             height={500}
                         />
-                        <LuArrowDown className="absolute bottom-[32%] left-[38%] transform -translate-x-1/2 text-4xl animate-bounce text-text" />
+                        <LuArrowDown className="absolute bottom-[32%] left-[38%] transform -translate-x-1/2 text-3xl sm:text-4xl animate-bounce text-text" />
                     </a>
                 </div>
             </div>
